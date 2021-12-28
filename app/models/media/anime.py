e1cdeb53a4103cafc2ad5bb373c4ad39401e9f38
BuildTools <unconfigured@null.spigotmc.org>
2021-12-28 13:21:47 +0200

    Fix anime typing

--- app/models/media/anime.py
+++ app/models/media/anime.py
@@ -7,5 +7,5 @@ from app.models.media.media_season import MediaSeason
 class Anime(Media):
     episodes: typing.Optional[int]
     duration: int
-    season: MediaSeason
-    seasonYear: int
+    season: typing.Optional[MediaSeason]
+    seasonYear: typing.Optional[int]
